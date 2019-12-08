4e85ca80042893f83209600fd7f6f735182036b8
keiohtani <35276788+keiohtani@users.noreply.github.com>
2019-12-08 13:37:54 -0500

    Remove unnecessary code

--- YT Music/Controllers/ViewController + MediaKeys.swift
+++ YT Music/Controllers/ViewController + MediaKeys.swift
@@ -15,15 +15,6 @@ import MediaPlayer
 
 extension ViewController: MediaKeyTapDelegate {
     func registerRemoteCommands() {
-        if #available(OSX 10.12.2, *) {
-            let commandCenter = MPRemoteCommandCenter.shared()
-            commandCenter.playCommand.addTarget(self, action: #selector(playPause))
-            commandCenter.pauseCommand.addTarget(self, action: #selector(playPause))
-            commandCenter.togglePlayPauseCommand.addTarget(self, action: #selector(playPause))
-            commandCenter.nextTrackCommand.addTarget(self, action: #selector(nextTrack))
-            commandCenter.previousTrackCommand.addTarget(self, action: #selector(previousTrack))
-            commandCenter.changePlaybackPositionCommand.addTarget(self, action: #selector(seek(_:)))
-        }
         mediaKeyTap = MediaKeyTap(delegate: self)
         mediaKeyTap?.start()
     }
