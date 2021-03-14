fd715ffb439f7e1e8de5d66ff928c311850bd050
Tim Oliver <me@timoliver.com.au>
2021-03-14 15:49:37 +0900

    Updated codebase to Swift 5 and cleaned warnings

--- YT Music/Views/CustomWindow.swift
+++ YT Music/Views/CustomWindow.swift
@@ -23,7 +23,7 @@ class CustomWindow: NSWindow {
         identifier = NSUserInterfaceItemIdentifier(rawValue: "main")
         backgroundColor = NSColor(hue:0.00, saturation:0.00, brightness:0.07, alpha:1.00)
         contentMinSize = NSSize(width: 800, height: 500)
-        setFrameAutosaveName(NSWindow.FrameAutosaveName(rawValue: "uk.co.wearecocoon.ytmusic.main"))
+        setFrameAutosaveName("uk.co.wearecocoon.ytmusic.main")
         
     }
     
