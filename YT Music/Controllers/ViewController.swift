e164bfe47a251fef430d7a4185f753cddafb3af7
Tony Li <atom2ueki@gmail.com>
2021-11-05 01:11:31 +0800

    make movable view height equal to 64

--- YT Music/Controllers/ViewController.swift
+++ YT Music/Controllers/ViewController.swift
@@ -78,7 +78,7 @@ class ViewController: NSViewController {
             standardButtonsView.addSubview(btn)
         }
         
-        movableView.frame = CGRect(x: 0, y: webView.isFlipped ? 0 : webView.frame.height - 20, width: webView.frame.width, height: 20)
+        movableView.frame = CGRect(x: 0, y: webView.isFlipped ? 0 : webView.frame.height - 20, width: webView.frame.width, height: 64)
         
         let y = webView.isFlipped ? 14 : webView.frame.height - 46
         
@@ -150,7 +150,8 @@ class ViewController: NSViewController {
     
     func addMovableView() {
         movableView = WindowMovableView(frame: .zero)
-        movableView.frame = CGRect(x: 0, y: 0, width: webView.frame.width, height: 20)
+        movableView.parent = webView
+        movableView.frame = CGRect(x: 0, y: 0, width: webView.frame.width, height: 64)
         webView.addSubview(movableView)
     }
     
