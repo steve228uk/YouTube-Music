b001f39209eb22c957dcf96c71438c4ca9b45592
Tony Li <atom2ueki@gmail.com>
2022-05-02 10:35:45 +0800

    revert draggable area back to 20px height (#162)

--- YT Music/Controllers/ViewController.swift
+++ YT Music/Controllers/ViewController.swift
@@ -78,7 +78,7 @@ class ViewController: NSViewController {
             standardButtonsView.addSubview(btn)
         }
         
-        movableView.frame = CGRect(x: 0, y: webView.isFlipped ? 0 : webView.frame.height - 20, width: webView.frame.width, height: 64)
+        movableView.frame = CGRect(x: 0, y: webView.isFlipped ? 0 : webView.frame.height - 20, width: webView.frame.width, height: 20)
         
         let y = webView.isFlipped ? 14 : webView.frame.height - 46
         
@@ -151,7 +151,7 @@ class ViewController: NSViewController {
     func addMovableView() {
         movableView = WindowMovableView(frame: .zero)
         movableView.parent = webView
-        movableView.frame = CGRect(x: 0, y: 0, width: webView.frame.width, height: 64)
+        movableView.frame = CGRect(x: 0, y: 0, width: webView.frame.width, height: 20)
         webView.addSubview(movableView)
     }
     
